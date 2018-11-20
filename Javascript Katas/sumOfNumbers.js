089119c623665d556ef3572d2dcb8b6e0f32ef41
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-11-20 14:44:06 -0500

    successful

--- Javascript Katas/sumOfNumbers.js
+++ Javascript Katas/sumOfNumbers.js
@@ -3,4 +3,25 @@
 // If the two numbers are equal return a or b.
 // Note: a and b are not ordered!
 
+-1 + 0 +1 +2 
 
+//Unknowns:  difference, sum and a ,b 
+
+
+
+const getSum = (a,b) => {
+    if ( a > b){
+
+        const n = (a-b) + 1
+        const sum = n * ((a + b)/2)
+        return sum
+        }
+        else if (b >a){
+            const n = (b-a) + 1
+            const sum = n * ((b + a)/2)
+            return sum
+        }else{
+            return a
+        }
+
+}
