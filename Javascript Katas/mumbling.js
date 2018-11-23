14e4ab27f0493b6d44480273c7d736bf717ec713
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-11-23 09:06:03 -0500

    successful kata

--- Javascript Katas/mumbling.js
+++ Javascript Katas/mumbling.js
@@ -4,5 +4,16 @@
 // The parameter of accum is a string which includes only letters from a..z and A..Z.
 
 const accum = (s) => {
-    
+    let output = '';
+     let c = null;
+     for (let i = 0; i < s.length; i++) {
+       c = s[i].toLowerCase();
+       for (let j = 0; j <= i; j++) {
+         output += (j === 0) ? c.toUpperCase() : c;
+       }
+       if (i < s.length-1) {
+        output += '-';
+      }
+    }
+    return output
 }
