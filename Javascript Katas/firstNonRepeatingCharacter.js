beea0ec3db230cbda3895dc2a2f43840a41cad78
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-11-27 12:16:38 -0500

    working on function

--- Javascript Katas/firstNonRepeatingCharacter.js
+++ Javascript Katas/firstNonRepeatingCharacter.js
@@ -10,5 +10,16 @@
 
 
 const firstNonRepeatingLetter = (s) => {
-    return s 
+   const length = s.length,
+      repeated = {};
+  for (var i = 0; i < length; i++) {
+    for (var j = i + 1; j < length; j++) {
+      if (s[i].toLowerCase() === s[j].toLowerCase()) {
+        repeated[s[i].toLowerCase()] = true;
+        break;
+      }
+    }
+    if (!repeated[s[i].toLowerCase()]) return s[i];
+  }
+ 
 }
