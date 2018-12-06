73955adb423884638f25bc1c0e16b29dfe454585
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-12-05 21:52:48 -0500

    successful kata

--- Javascript Katas/supermarketqueue.js
+++ Javascript Katas/supermarketqueue.js
@@ -29,5 +29,16 @@
 
 
 const queueTime = (customers, n) => {
-    //
+    if (n < 1) return 0;
+    if (n === 1) return customers.reduce((acc, customer) => acc + customer, 0);
+  
+    const head = customers.slice(0, n);
+    const tail = customers.slice(n);
+    const lines = tail.reduce((acc, customer) => {
+      const minIndex = acc.indexOf(Math.min(...acc));
+      acc[minIndex] += customer;
+      return acc;
+    }, head);
+  
+    return Math.max(...lines);
   }
