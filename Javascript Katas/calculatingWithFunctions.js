273cbd2b351eb740ea16c042af9829a2c0f65177
Adit Patel <MyFolder@Adits-MacBook-Air.local>
2018-12-09 17:56:09 -0500

    failed attempt

--- Javascript Katas/calculatingWithFunctions.js
+++ Javascript Katas/calculatingWithFunctions.js
@@ -19,3 +19,40 @@
 // Each calculation consist of exactly one operation and two numbers
 // The most outer function represents the left operand, the most inner function represents the right operand
 // Divison should be integer division, e.g eight(dividedBy(three()))/eight(divided_by(three)) should return 2, not 2.666666...
+
+
+//Passed sample test but failed actual attempt.
+
+
+// function recur(n, op) { return (op) ? op.call(op, n) : n; }
+// function zero(op)     { return recur(0, op); }
+// function one(op)      { return recur(1, op); }
+// function two(op)      { return recur(2, op); }
+// function three(op)    { return recur(3, op); }
+// function four(op)     { return recur(4, op); }
+// function five(op)     { return recur(5, op); }
+// function six(op)      { return recur(6, op); }
+// function seven(op)    { return recur(7, op); }
+// function eight(op)    { return recur(8, op); }
+// function nine(op)     { return recur(9, op); }
+
+// function plus(num) {
+//     return function(res) {
+//         return res + num;
+//     };
+// }
+// function minus(num) {
+//     return function(res) {
+//         return res - num;
+//     };
+// }
+// function times(num) {
+//     return function(res) {
+//         return res * num;
+//     };
+// }
+// function dividedBy(num) {
+//     return function(res) {
+//         return res / num;
+//     };
+// }
